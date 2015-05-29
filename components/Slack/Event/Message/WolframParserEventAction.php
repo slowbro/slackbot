c15d3d53bfa256f0cb17d05dd09b1d7c88d17196
Katelyn Schiesser <katelyn.schiesser@gmail.com>
2015-05-29 19:05:45 +0000

    catch exceptions in WolframParser

--- components/Slack/Event/Message/WolframParserEventAction.php
+++ components/Slack/Event/Message/WolframParserEventAction.php
@@ -6,17 +6,23 @@ class WolframParserEventAction extends DefaultEventAction {
     protected $regex = '#^((what|when|where|why|how|who|which)((\')?s)?|(are|is|do))\s#';
 
     public function run(){
-        $appId = \slackbot\models\Config::getValue('wa.appid');
-        $wa = new \ConnorVG\WolframAlpha\WolframAlpha($appId);
-        $ans = $wa->easyQuery($this->getCleanText());
-        $dym = $wa->didyoumean;
-        if(is_array($ans)){
-            $a = preg_replace('#(Result|Response):#', '', $ans[0]);
-            $this->message->reply(trim($a));
-        } else {
-            $this->message->reply("I don't know :(");
+        try {
+            $appId = \slackbot\models\Config::getValue('wa.appid');
+            $wa = new \ConnorVG\WolframAlpha\WolframAlpha($appId);
+            $ans = $wa->easyQuery($this->getCleanText());
+            $dym = $wa->didyoumean;
+            if(is_array($ans)){
+                $a = preg_replace('#(Result|Response):#', '', $ans[0]);
+                $this->message->reply(trim($a));
+            } else {
+                $this->message->reply("I don't know :(");
+            }
+        } catch (\Exception $e){
+            $this->message->reply("Something went wrong: ".$e->getMessage());
+            echo $e;
+        } finally {
+            throw new \Slack\Exception\StopProcessingException;
         }
-        throw new \Slack\Exception\StopProcessingException;
     }
 
 }
