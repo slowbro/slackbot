20a1b87785588e7e9c0c5e34df781a5ab7120481
Katelyn Schiesser <katelyn.schiesser@gmail.com>
2015-07-28 23:32:15 +0000

    remove old Slack

--- components/Slack/Exception/SkipActionException.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Slack\Exception;
-
-class SkipActionException extends \Exception {
-
-    public function __construct($message = "SkipAction called."){
-        parent::__construct($message);
-    }
-
-}
--- components/Slack/Exception/StopProcessingException.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Slack\Exception;
-
-class StopProcessingException extends \Exception {
-
-    public function __construct($message = "StopProcessing called."){
-        parent::__construct($message);
-    }
-
-}
